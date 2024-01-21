157667f703083a07fea234407a47ff67caba605c
dhearr <dhearr27@gmail.com>
2024-01-21 13:39:28 +0700

    first update

---
+++ script.js
@@ -0,0 +1,18 @@
+// =====================================================================================================
+// // EVENTS
+// // EVENT HENDLER
+// const p3 = document.querySelector(".p3");
+// p3.onclick = function () {
+//   p3.style.backgroundColor = "lightblue";
+// };
+
+// // addEventListener()
+// const p4 = document.querySelector("section#b p");
+// p4.addEventListener("click", function () {
+//   const ul = document.querySelector("section#b ul");
+//   const itemBaru = document.createElement("li");
+//   const textLiBaru = document.createTextNode("item baru");
+//   itemBaru.appendChild(textLiBaru);
+//   ul.appendChild(itemBaru);
+// });
+// =====================================================================================================
